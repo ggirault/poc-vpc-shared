a7dff2666299784507b498e628c594c1ca76b230
ggirault <73304700+ggirault@users.noreply.github.com>
2021-05-21 17:17:38 +0200

    fix ingress traffic to allow icmp

--- ec2.tf
+++ ec2.tf
@@ -100,8 +100,8 @@ resource "aws_security_group" "allow_internet_icmp" {
   }
 
   egress {
-    from_port   = -1
-    to_port     = -1
+    from_port   = 0
+    to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
